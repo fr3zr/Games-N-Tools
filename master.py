41443b5e09bc76801a0cc3ae381cbaea169eb1a3
fr3zr <georgebasler2@gmail.com>
2019-12-06 14:27:58 +0100

    Implemented Choice Tree

--- master.py
+++ master.py
@@ -4,4 +4,57 @@ import os
 import sys
 import numGuess
 
-numGuess
+c = 1
+
+while c == 1:
+    a = 0
+    c = 1
+    print('Welcome to the Games' + "'" + 'n' + "'" 'Tools program')
+    print('')
+    print('Hello, what is your name?')
+    myName = input('My name is : ')
+    print('Hello, ' + myName +'!')
+    print()
+    print('Please select a Category from below or type exit to cancel')
+    print('[1] Games')
+    print('[2] Tools')
+    a = input()
+    a = int(a)
+    if a == 1:
+        print('Please choose from one of the options down below')
+        print()
+        print('[1] Number Guessing Game')
+        print()
+        b = input()
+        b = int(b)
+        if b == 1:
+            answer = 'y'
+            while answer == 'y'
+                tries = 0
+                num = random.randint(1,20)
+                numGuessed = 0
+                while numGuessed != num:
+                    tries = tries + 1
+                    if tries == 1:
+                        print('I choose a number between 1 and 20')
+                    elif tries > 1:
+                        if numGuessed > num:
+                            print('My number is smaller than the one you guessed')
+                        elif numGuessed < num:
+                            print('My number is bigger than the one you guessed')
+                    print('What number are you guessing?')
+                    numGuessed = input('My guess is: ')
+                    numGuessed = int(numGuessed)
+                    if numGuessed < 1 or numGuessed > 20:
+                        print('I said a number between 1 and 20')
+                if numGuessed == num:
+                    tries = str(tries)
+                    print('You guessed the number correctly')
+                    print('It took you ' + tries + ' tries to guess the number')
+                    answer = input('Do you want to try again? y/n : ')
+                else:
+                    print('You somehow created a error, please tell the administrator the steps to reproduce it')
+            print('Exiting Number guessing game')
+        else:
+            print('You made an Error')
+        pass
--- numGuess.py
+++ numGuess.py
@@ -2,33 +2,34 @@
 
 import random
 
-print('Hello, what is your name?')
-myName = input('My name is : ')
-print('Hello, ' + myName +'!')
-answer = 'y'
-while answer == 'y':
-    tries = 0
-    num = random.randint(1,20)
-    numGuessed = 0
-    while numGuessed != num:
-        tries = tries + 1
-        if tries == 1:
-            print('I choose a number between 1 and 20')
-        elif tries > 1:
-            if numGuessed > num:
-                print('My number is smaller than the one you guessed')
-            elif numGuessed < num:
-                print('My number is bigger than the one you guessed')
-        print('What number are you guessing?')
-        numGuessed = input('My guess is: ')
-        numGuessed = int(numGuessed)
-        if numGuessed < 1 or numGuessed > 20:
-            print('I said a number between 1 and 20')
-    if numGuessed == num:
-        tries = str(tries)
-        print('You guessed the number correctly')
-        print('It took you ' + tries + ' tries to guess the number')
-        answer = input('Do you want to try again? y/n : ')
-    else:
-        print('You somehow created a error, please tell the administrator the steps to reproduce it')
-print('Exiting Number guessing game')
+def numGuessed():
+    print('Hello, what is your name?')
+    myName = input('My name is : ')
+    print('Hello, ' + myName +'!')
+    answer = 'y'
+    while answer == 'y':
+        tries = 0
+        num = random.randint(1,20)
+        numGuessed = 0
+        while numGuessed != num:
+            tries = tries + 1
+            if tries == 1:
+                print('I choose a number between 1 and 20')
+            elif tries > 1:
+                if numGuessed > num:
+                    print('My number is smaller than the one you guessed')
+                elif numGuessed < num:
+                    print('My number is bigger than the one you guessed')
+            print('What number are you guessing?')
+            numGuessed = input('My guess is: ')
+            numGuessed = int(numGuessed)
+            if numGuessed < 1 or numGuessed > 20:
+                print('I said a number between 1 and 20')
+        if numGuessed == num:
+            tries = str(tries)
+            print('You guessed the number correctly')
+            print('It took you ' + tries + ' tries to guess the number')
+            answer = input('Do you want to try again? y/n : ')
+        else:
+            print('You somehow created a error, please tell the administrator the steps to reproduce it')
+    print('Exiting Number guessing game')
